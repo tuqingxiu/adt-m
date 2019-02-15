619de8047c135baaa03693c89d708b5912453923
涂青秀 <631795289@qq.com>
2019-02-15 18:00:48 +0800

    1

--- my-js/common.js
+++ my-js/common.js
@@ -1,38 +1,44 @@
 (function($) {
+    //header.html加载成功
+    function headerSuccess(){
+        var current = $(this).attr('data-type');
+        $('header').find('[data-type='+current+']').addClass('current');
+        //监听语言切换
+        $('.language[type]').click(function(){
+            var type = $(this).attr('type');
+            if(type == 'ch'){//中文
+                var href = location.href.replace('-en.html','.html');
+                location.href = href;
+            }else if(type == 'en'){//英文
+                var href = location.href.replace('.html','-en.html');
+                location.href = href;
+            }
+        })
+        //显示菜单
+        $('.main-header .main-menu-btn').click(function(){
+            $('#headerBox .main-menu').fadeIn();
+        })
+        //隐藏菜单
+        $('#headerBox .menu-mask').click(function(){
+            $('#headerBox .main-menu').fadeOut();
+        })
+    }
     function loadHeader(){
         var isEn = location.pathname.indexOf('-en.html')>=0;//英文版
         if(isEn){
             $("#headerBox").load("common/header-en.html",function(){
-                var current = $(this).attr('data-type');
-                $('header').find('[data-type='+current+']').addClass('current');
-                //监听语言切换
-                $('.language[type]').click(function(){
-                    var type = $(this).attr('type');
-                    if(type == 'ch'){//中文
-                        var href = location.href.replace('-en.html','.html');
-                        location.href = href;
-                    }
-                })
+                headerSuccess();
             });
             $("#footerBox").load("common/footer-en.html");
         }else{
             $("#headerBox").load("common/header.html",function(){
-                var current = $(this).attr('data-type');
-                $('header').find('[data-type='+current+']').addClass('current');
-                //监听语言切换
-                $('.language[type]').click(function(){
-                    var type = $(this).attr('type');
-                    if(type == 'en'){//英文
-                        var href = location.href.replace('.html','-en.html');
-                        location.href = href;
-                    }
-                })
+                headerSuccess();
             });
             $("#footerBox").load("common/footer.html");
         }
     }
+    
     $(window).on('load', function() {
         loadHeader();
-        
 	});	
 })(window.jQuery);
