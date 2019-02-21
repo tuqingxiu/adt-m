bda7e0d26f14228cbe36fcb14131a33235b9e63e
涂青秀 <631795289@qq.com>
2019-02-21 17:57:40 +0800

    企业文化

--- my-js/culture.js
+++ my-js/culture.js
@@ -4,66 +4,8 @@ $(function() {
 		setHeaderShow();
 	});
     goDesignatedLocation();
-    //文化的力量居中
-    textCenter();
-    animate1();
-    animate2();
-    animate3();
-})
-//文化的力量居中
-function textCenter(){
-    var h1 = $('.section4 .item1').height();
-    var h2 = $('.section4 .item2 .cont2>div').height();
-    var h = (h1-h2)/2;
-    if(isIE()){
-        h = h/2;
-    }
-    $('.section4 .item2 .cont2>div').css('margin-top',h);
-}
-//第三屏遮罩层显示，文化项目
-function animate1(){
-    $('.section3 .boxs').hover(function(){
-        showUp($(this).find('.content-box'));
-    },function(){
-        hideDown($(this).find('.content-box'));
-    })
-}
-//字体放大
-function animate2(){
-    $('.animated-text').hover(function(){
-        $(this).addClass('scale-text');
-    },function(){
-        $(this).removeClass('scale-text');
-    })
-}
-//蓝天计划 显示遮罩层
-function animate3(){
-    $('.section7 .img-content .item-box').hover(function(){
-        showUp($(this).find('.content-box'));
-        //文字超出时滚动
-        var allH = $(this).find('.cont-box').height();
-        var contH1 = $(this).find('.cont1').height();
-        var contH2 = $(this).find('.cont2').height();
-        var marquee = $(this).find('.cont2');
 
-        if(allH<contH1+contH2){
-            marquee.attr('scrollamount',5);
-            marquee[0].start();
-        }
-    },function(){
-        hideDown($(this).find('.content-box'));
-        var marquee = $(this).find('.cont2');
-        marquee[0].stop();
-    })
-}
-//显示遮罩层
-function showUp(el){
-    el.fadeIn().removeClass('fadeOutDown').addClass('fadeInUp');
-}
-//隐藏遮罩层
-function hideDown(el){
-    el.removeClass('fadeInUp').addClass('fadeOutDown').fadeOut();
-}
+})
 
 //跳转到指定位置
 function getUrlParam(name){
@@ -78,9 +20,6 @@ function goDesignatedLocation(){
     if(item && el.length){
         var scroll = el.offset().top;
         $(window).scrollTop(scroll);
-        // setTimeout(function(){
-        // 	$(window).scrollTop(scroll);
-        // },1000)
     }
 }
 
